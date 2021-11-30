10723deca3c460b3cd03bc73a4e1e455b6389038
IchsnArrzq <ichsanarrizqi@smkwikrama.sch.id>
2021-11-30 14:55:23 +0700

    auth sanctum

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\Api\AduanController;
+use App\Http\Controllers\Api\InformasiController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Validation\ValidationException;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +21,32 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::post('login', [LoginController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function(){
+    Route::get('/informasi',[InformasiController::class, 'index']);
+    Route::resource('/aduan', AduanController::class);
+});
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+Route::post('/sanctum/token', function (Request $request) {
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+        // 'device_name' => 'required',
+    ]);
+
+    $user = User::where('email', $request->email)->first();
+
+    if (! $user || ! Hash::check($request->password, $user->password)) {
+        throw ValidationException::withMessages([
+            'email' => ['The provided credentials are incorrect.'],
+        ]);
+    }
+
+    return $user->createToken('web')->plainTextToken;
 });
